e3bc678f5db1177d4873d89bc5dfb91e91bbec67
arrayryry <emiliagajfullina6@gmail.com>
2026-05-18 15:56:45 +0300

    исправление ошибок

--- backend/graph_api/serializers.py
+++ backend/graph_api/serializers.py
@@ -1,26 +1,28 @@
-# api/serializers.py
 from rest_framework import serializers
 
-class NodeSerializer(serializers.Serializer):
-    id = serializers.CharField(max_length=50)
-    rus_word = serializers.CharField(max_length=50)
-    eng_word = serializers.CharField(max_length=50)
-    node_type = serializers.CharField(max_length=30)
+class ConceptSerializer(serializers.Serializer):
+    id = serializers.IntegerField() 
+    ru_name = serializers.CharField(max_length=500)
+    en_name = serializers.CharField(max_length=500)
+    type = serializers.CharField(max_length=50)  # object_concept, action_concept, object_attribute_concept, action_attribute_concept
+    hypernym = serializers.IntegerField(allow_null=True, required=False)  # id родителя
 
-class NodeWithNeighborsSerializer(serializers.Serializer):
-    id = serializers.CharField()
-    rus_word = serializers.CharField(max_length=50)
-    eng_word = serializers.CharField(max_length=50)
-    node_type = serializers.CharField()
-    neighbors = serializers.ListField(child=serializers.DictField())
+class ConceptWithNeighborsSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    ru_name = serializers.CharField()
+    en_name = serializers.CharField()
+    type = serializers.CharField()
+    hypernym = serializers.IntegerField(allow_null=True)
+    children = serializers.ListField(child=serializers.DictField())  # дочерние концепты
+    semantic_neighbors = serializers.ListField(child=serializers.DictField())  # синонимы, антонимы и т.д.
 
 class CreateEdgeSerializer(serializers.Serializer):
-    from_id = serializers.CharField()
-    to_id = serializers.CharField()
+    from_id = serializers.IntegerField()
+    to_id = serializers.IntegerField()
     rel_type = serializers.CharField()
 
-class UpdateNodeSerializer(serializers.Serializer):
-    new_id = serializers.CharField(required=False)
-    new_rus_word = serializers.CharField(max_length=50, required=False)
-    new_eng_word = serializers.CharField(max_length=50, required=False)
-    new_node_type = serializers.CharField(max_length=30, required=False)
+class UpdateConceptSerializer(serializers.Serializer):
+    new_ru_name = serializers.CharField(max_length=500, required=False)
+    new_en_name = serializers.CharField(max_length=500, required=False)
+    new_type = serializers.CharField(max_length=50, required=False)
+    new_hypernym = serializers.IntegerField(allow_null=True, required=False)
